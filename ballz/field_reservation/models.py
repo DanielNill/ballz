eb227190cb3f1fed0f73b50bdcdbb6bcb58e6ef6
Brian Cooksey <bac2k@hotmail.com>
2012-03-27 21:22:23 -0500

    Add stringificaiton to models

--- ballz/field_reservation/models.py
+++ ballz/field_reservation/models.py
@@ -4,7 +4,13 @@ class Field(models.Model):
     name = models.CharField(max_length=65)
     location = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return self.name
+
 class Reservation(models.Model):
     field = models.ForeignKey(Field)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+
+    def __unicode__(self):
+        return self.field
