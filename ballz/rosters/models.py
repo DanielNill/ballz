288e2a6b58ebaf0ba3e2a065ad74c57cb02e0a63
Daniel Nill <DanielLNill@gmail.com>
2012-03-27 20:58:35 -0500

    fixed some models

--- ballz/rosters/models.py
+++ ballz/rosters/models.py
@@ -1,24 +1,30 @@
 from django.db import models
 
-class Player(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField(blank=True, null=True)
-    position = models.CharField(max_length=100, blank=True, null=True)
-    contact_info = models.ForeignKey(ContactInfo)
-    photo = models.ImageField(blank=True, null=True)
-    stats = models.ForeignKey(Stats)
-    
 class ContactInfo(models.Model):
     phone = models.CharField(max_length=11, blank=True, null=True)
-    email = models.EmailField(blank=True, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
     address = models.CharField(max_length=150, blank=True, null=True)
     city = models.CharField(max_length=50, blank=True, null=True)
     state = models.CharField(max_length=15, blank=True, null=True)
     zipcode = models.IntegerField(blank=True, null=True)
     
+    def __unicode__(self):
+        return self.phone
+
+class StatTypes(models.Model):
+    name = models.CharField(max_length=25)
+
 class Stat(models.Model):
     stat_type = models.ForeignKey(StatTypes)
-    stat_value = models.DoubleField()
+    stat_value = models.IntegerField()
     
-class StatTypes(models.Model):
-    name = models.CharField(max_length=25)        
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(blank=True, null=True)
+    position = models.CharField(max_length=100, blank=True, null=True)
+    contact_info = models.ForeignKey(ContactInfo)
+    photo = models.ImageField(upload_to='media/player_photos/', blank=True, null=True)
+    stats = models.ForeignKey(Stat)
+
+    def __unicode__(self):
+        return self.name
