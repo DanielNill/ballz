0d223cbbe855cc98467dbf81475e11ddbd2e89d0
Daniel Nill <DanielLNill@gmail.com>
2012-04-10 22:57:57 -0500

    merging stuff

--- ballz/rosters/models.py
+++ ballz/rosters/models.py
@@ -28,10 +28,7 @@ class Stat(models.Model):
     def __unicode__(self):
         return self.state_type
 
-<<<<<<< HEAD
 
-=======
->>>>>>> fbede578d2756c93560071cfd2e44af5010459bc
 class Player(models.Model):
     '''
     TODO: change postion so that you can have auto suggest or assign multiple positions to one player
@@ -42,14 +39,10 @@ class Player(models.Model):
     position = models.CharField(max_length=100, blank=True, null=True)
     contact_info = models.ForeignKey(ContactInfo)
     photo = models.ImageField(upload_to='media/player_photos/', blank=True, null=True)
-<<<<<<< HEAD
+
     stats = models.ForeignKey(Stat, blank=True, null=True)
     created_by = models.ForeignKey(User)
 
-=======
-    stats = models.ForeignKey(Stat)
-    user = models.ForeignKey(User, default=0) #the user that input that player
->>>>>>> fbede578d2756c93560071cfd2e44af5010459bc
     def __unicode__(self):
         return self.name
 
@@ -57,15 +50,8 @@ class Player(models.Model):
 class Sport(models.Model):
     name = models.CharField(max_length=100)
 
-<<<<<<< HEAD
 
-=======
->>>>>>> fbede578d2756c93560071cfd2e44af5010459bc
 class Team(models.Model):
     name = models.CharField(max_length=50, blank=True, null=True)
     players = models.ManyToManyField(Player)
     sport = models.ForeignKey(Sport)
-<<<<<<< HEAD
-=======
-
->>>>>>> fbede578d2756c93560071cfd2e44af5010459bc
